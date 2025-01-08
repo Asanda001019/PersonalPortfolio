7eaf293174ca0d321b93587ea91703ef8f0aef23
User <User@WRSD-LD309-018.dut.ac.za>
2025-01-08 15:01:21 +0200

    feat:skills page done

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import TeamSection from './components/TeamSection';
 import Resume from './components/Resume';
 import Skills from './components/Skills';
 import './App.css';
+import UserProfile from './components/UserProfile';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div>
-      <TeamSection />
+      <UserProfile/>
       <Resume />
       <Skills />
+      <Projects />
+      <TeamSection />
+     
+     
       {/* Other components can go here */}
     </div>
   );
